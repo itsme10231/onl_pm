c483b572eee40971f93c92b8774bdeb781de3c73
hk_edu <hk_edu@192.168.2.103>
2020-05-28 17:56:24 +0900

    chatMapper작성

--- onl_pm/src/main/java/com/nl/onl/dtos/ChatDto.java
+++ onl_pm/src/main/java/com/nl/onl/dtos/ChatDto.java
@@ -1,5 +1,105 @@
 package com.nl.onl.dtos;
 
+import java.util.Date;
+
 public class ChatDto {
 
+	private int seq;
+	private String receive_id;
+	private String send_id;
+	private String content;
+	private Date chatdate;
+	private String chkflag;
+	private String delflag;
+	private int wanted_seq;
+	
+	public ChatDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ChatDto(int seq, String receive_id, String send_id, String content, Date chatdate, String chkflag,
+			String delflag, int wanted_seq) {
+		super();
+		this.seq = seq;
+		this.receive_id = receive_id;
+		this.send_id = send_id;
+		this.content = content;
+		this.chatdate = chatdate;
+		this.chkflag = chkflag;
+		this.delflag = delflag;
+		this.wanted_seq = wanted_seq;
+	}
+
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	public String getReceive_id() {
+		return receive_id;
+	}
+
+	public void setReceive_id(String receive_id) {
+		this.receive_id = receive_id;
+	}
+
+	public String getSend_id() {
+		return send_id;
+	}
+
+	public void setSend_id(String send_id) {
+		this.send_id = send_id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getChatdate() {
+		return chatdate;
+	}
+
+	public void setChatdate(Date chatdate) {
+		this.chatdate = chatdate;
+	}
+
+	public String getChkflag() {
+		return chkflag;
+	}
+
+	public void setChkflag(String chkflag) {
+		this.chkflag = chkflag;
+	}
+
+	public String getDelflag() {
+		return delflag;
+	}
+
+	public void setDelflag(String delflag) {
+		this.delflag = delflag;
+	}
+
+	public int getWanted_seq() {
+		return wanted_seq;
+	}
+
+	public void setWanted_seq(int wanted_seq) {
+		this.wanted_seq = wanted_seq;
+	}
+
+	@Override
+	public String toString() {
+		return "ChatDto [seq=" + seq + ", receive_id=" + receive_id + ", send_id=" + send_id + ", content=" + content
+				+ ", chatdate=" + chatdate + ", chkflag=" + chkflag + ", delflag=" + delflag + ", wanted_seq="
+				+ wanted_seq + "]";
+	}
+	
 }
--- onl_pm/src/test/java/com/nl/onl/AppTest_tae.java
+++ onl_pm/src/test/java/com/nl/onl/AppTest_tae.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.nl.onl.dtos.ChatDto;
 import com.nl.onl.dtos.ReviewDto;
 import com.nl.onl.dtos.WantedDto;
 
@@ -24,79 +25,123 @@ public class AppTest_tae {
 	@Autowired
 	SqlSessionTemplate sqlSession;
 	
+//	마이페이지 테스트
+//	@Test
+//	public void test1() {
+//		
+//		
+////		System.out.println("Test");
+////		System.out.println("sqlSession: " +sqlSession);
+//		
+//		String id = "onltest1";
+//		List<WantedDto> list = sqlSession.selectList("com.nl.onl.schedule.worked", id);
+//		
+//		for(WantedDto wdto:list) {
+//			System.out.println("1. "+wdto);
+//		}
+//	}
+//	
+//	@Test
+//	public void test2() {
+//		
+//		String id = "onltest1";
+//		List<WantedDto> list = sqlSession.selectList("com.nl.onl.myPage.detail", id);
+//		
+//		for(WantedDto wdto:list) {
+//			System.out.println("2. "+wdto);
+//		}
+//	}
+//	
+//	@Test
+//	public void test3() {	
+//		Map<String, String> map=new HashMap<String, String>();
+//		map.put("id", "onltest1");
+//		map.put("pnum", "1");
+//		sqlSession.selectList("com.nl.onl.myPage.getAllMyList", map);
+//		
+//		List<WantedDto> wdto3 = sqlSession.selectList("com.nl.onl.myPage.getAllMyList", map);
+//		
+//		for(WantedDto wdto:wdto3) {
+//			System.out.println("3. "+wdto);
+//		}
+//	}	
+//	
+//	@Test
+//	public void test4() {
+//		Map<String, Integer> map=new HashMap<String, Integer>();
+//		map.put("selector", 1);
+//		map.put("seq", 1);
+//		
+//		List<WantedDto> wdto4 = sqlSession.selectList("com.nl.onl.myPage.selector", map);
+//		
+//		for(WantedDto wdto:wdto4) {
+//			System.out.println("4. "+wdto);
+//		}
+//	}
+//	
+//	@Test
+//	public void test5() {
+//		Map<String, String> map=new HashMap<String, String>();
+//		map.put("id", "onltest1");
+//		map.put("type", "OFFER");
+//		map.put("pnum", "1");
+//		
+//		List<ReviewDto> wdto5 = sqlSession.selectList("com.nl.onl.myPage.getReview", map);
+//		
+////		System.out.println(wdto5);
+//		for(ReviewDto wdto:wdto5) {
+//			System.out.println("5. "+wdto);
+//		}
+//	}
+//	
+//	@Test
+//	public void test6() {
+//		
+//		int pcount = sqlSession.selectOne("com.nl.onl.myPage.pcount");
+//		
+//		System.out.println("6." +pcount);
+//		
+////		for(WantedDto wdto:wdto6) {
+////			System.out.println("6. "+wdto);
+////		}
+//	}
+	
+	
+//	채팅테스트
 	@Test
 	public void test1() {
+		Map<String, String> map=new HashMap<String, String>();
+		map.put("receive_id", "onltest1");
+		map.put("send_id", "onltest2");
 		
+		List<ChatDto> wdto1 = sqlSession.selectList("com.nl.onl.chat.getMessage", map);
 		
-//		System.out.println("Test");
-//		System.out.println("sqlSession: " +sqlSession);
-		
-		String id = "onltest1";
-		List<WantedDto> list = sqlSession.selectList("com.nl.onl.schedule.worked", id);
-		
-		for(WantedDto wdto:list) {
+		for(ChatDto wdto:wdto1) {
 			System.out.println("1. "+wdto);
 		}
 	}
+	
 	@Test
 	public void test2() {
+		Map<String, String> map=new HashMap<String, String>();
+		map.put("chkflag", "Y");
+		map.put("receive_id", "onltest1");
 		
-		String id = "onltest1";
-		List<WantedDto> list = sqlSession.selectList("com.nl.onl.myPage.detail", id);
+		List<ChatDto> wdto2 = sqlSession.selectList("com.nl.onl.chat.checkChkflag", map);
 		
-		for(WantedDto wdto:list) {
+		for(ChatDto wdto:wdto2) {
 			System.out.println("2. "+wdto);
 		}
 	}
-	@Test
-	public void test3() {	
-		Map<String, String> map=new HashMap<String, String>();
-		map.put("id", "onltest1");
-		map.put("pnum", "1");
-		sqlSession.selectList("com.nl.onl.myPage.getAllMyList", map);
-		
-		List<WantedDto> wdto3 = sqlSession.selectList("com.nl.onl.myPage.getAllMyList", map);
-		
-		for(WantedDto wdto:wdto3) {
-			System.out.println("3. "+wdto);
-		}
-	}	
 	
 	@Test
-	public void test4() {
-		Map<String, Integer> map=new HashMap<String, Integer>();
-		map.put("selector", 1);
-		map.put("seq", 1);
+	public void test3() {
+		ChatDto cdto=new ChatDto(0, "onltest1", "onltest3", "테스트1이 테스트3한테 전송", null, "N", "N", 5);
 		
-		List<WantedDto> wdto4 = sqlSession.selectList("com.nl.onl.myPage.selector", map);
+		int isS = sqlSession.insert("com.nl.onl.chat.sendMessage", cdto);
 		
-		for(WantedDto wdto:wdto4) {
-			System.out.println("4. "+wdto);
-		}
-	}
-	
-	@Test
-	public void test5() {
-		Map<String, String> map=new HashMap<String, String>();
-		map.put("id", "onltest1");
-		map.put("type", "SEARCH");
-		map.put("pnum", "1");
-		
-		List<ReviewDto> wdto5 = sqlSession.selectList("com.nl.onl.myPage.getReview", map);
-		
-		for(ReviewDto wdto:wdto5) {
-			System.out.println("5. "+wdto);
-		}
-	}
-	@Test
-	public void test6() {
-		
-		int pcount = sqlSession.selectOne("com.nl.onl.myPage.pcount");
-		
-		System.out.println("6." +pcount);
-		
-//		for(WantedDto wdto:wdto6) {
-//			System.out.println("6. "+wdto);
+//		for(ChatDto wdto:isS) {
+//			System.out.println("3. "+wdto);
 //		}
 	}
 }
